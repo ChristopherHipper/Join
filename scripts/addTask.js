7377df369bc0d4c6ba5b571fa661cfb538f7f2e5
Christopher Hipper <hipper.christopher@gmail.com>
2025-05-10 16:56:22 +0200

    update add tasks

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,3 +1,9 @@
+let title = document.getElementById('title')
+let description = document.getElementById('description')
+let date = document.getElementById('date')
+let category = ""
+let priority = "medium";
+
 function initTask() {
     getFromLocalStorage()
     highlightLink()
@@ -7,9 +13,28 @@ function highlightLink() {
     document.getElementById('addTask_link').classList.add('aktive_link')
 }
 
-function addSubtask(){
+function addSubtask() {
     let subtaskRef = document.getElementById('subtask_input')
     let subtaskContent = document.getElementById('subtask')
     subtaskContent.innerHTML += `- ` + subtaskRef.value + `<br>`
     subtaskRef.value = ""
 };
+
+function selectedPrio(prio) {
+    priority = "";
+    switch (prio) {
+        case "urgent":
+            return priority = "urgent"
+        case "medium":
+            return priority = "medium"
+        case "low":
+            return priority = "low"
+        default:
+            break;
+    }
+}
+
+function selectedCategory() {
+    let selectCategory = document.getElementById('category')
+    category = selectCategory.options[selectCategory.selectedIndex].text
+}
--- scripts/data.js
+++ scripts/data.js
@@ -6,24 +6,32 @@ let tasks = [
         "name": "kochen",
         "description": "essen machen",
         "id": 0,
-        "category": "toDo"
+        "category": "toDo",
+        "prio": "",
+        "date": "1.1.2001"
     },
     {
         "name": "saugen",
         "description": "boden saugen",
         "id": 1,
-        "category": "inProgress"
+        "category": "inProgress",
+        "prio": "inProgress",
+        "date": "1.1.2001"
     },
     {
         "name": "duschen",
         "description": "sich duschen",
         "id": 2,
-        "category": "toDo"
+        "category": "toDo",
+        "prio": "toDo",
+        "date": "1.1.2001"
     },
     {
         "name": "schlafen",
         "description": "ins bett legen",
         "id": 3,
-        "category": "await"
+        "category": "await",
+        "prio": "await",
+        "date": "1.1.2001"
     },
 ]
